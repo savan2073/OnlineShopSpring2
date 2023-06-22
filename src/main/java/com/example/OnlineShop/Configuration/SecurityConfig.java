80f4fd81c0153f027a120b863e8414e155841153
bartoszsawaniewski <savan2073@gmail.com>
2023-06-22 21:58:13 +0200

    wylogowywanie działa nie do końca

--- src/main/java/com/example/OnlineShop/Configuration/SecurityConfig.java
+++ src/main/java/com/example/OnlineShop/Configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.OnlineShop.Configuration;
 
 import com.example.OnlineShop.Model.User;
 import com.example.OnlineShop.Services.UserDetailsServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -43,6 +45,13 @@ public class SecurityConfig{
                 .formLogin((login) -> login
                         .permitAll()
                         .defaultSuccessUrl("/", true)
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll()
                 );
 
         return http.build();
